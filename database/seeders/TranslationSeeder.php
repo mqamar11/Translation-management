e04a9fd4a161394069267f4d47dfa763c5cede9d
mqamar11 <qamarmaqsood11@gmail.com>
2025-08-03 13:39:02 -0700

    seeder change

--- database/seeders/TranslationSeeder.php
+++ database/seeders/TranslationSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Translation;
+use App\Models\Tag;
 
 class TranslationSeeder extends Seeder
 {
@@ -13,6 +14,14 @@ class TranslationSeeder extends Seeder
      */
     public function run(): void
     {
-        Translation::factory()->count(100000)->create();
+        $tags = Tag::factory()->count(10)->create();
+        Translation::factory()
+        ->count(100000)
+        ->create()
+        ->each(function ($translation) use ($tags) {
+            $translation->tags()->attach(
+                $tags->random(rand(1, 3))->pluck('id')->toArray()
+            );
+        });
     }
 }
