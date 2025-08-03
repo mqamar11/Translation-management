b5aeab7a1ef5e9b685590796b2607d875cc3e07d
mqamar11 <qamarmaqsood11@gmail.com>
2025-08-03 13:11:29 -0700

    seeders

--- database/seeders/TranslationSeeder.php
+++ database/seeders/TranslationSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Translation;
 
 class TranslationSeeder extends Seeder
 {
@@ -12,6 +13,6 @@ class TranslationSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Translation::factory()->count(100000)->create();
     }
 }
